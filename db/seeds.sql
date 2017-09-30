2ee68dd2f5a195f60b0cbcf6b098262cb3fee74b
KKShah123 <kundankshah@gmail.com>
2017-09-30 17:28:11 -0600

    Changes

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom  Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+
+
