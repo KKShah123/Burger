f5663b922d8e44f1ece5f1638a14e9d8ba82ece3
KKShah123 <kundankshah@gmail.com>
2017-09-30 17:52:51 -0600

    Hooking Project with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //Connet to mysql and export the connection
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Kundan123#",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
